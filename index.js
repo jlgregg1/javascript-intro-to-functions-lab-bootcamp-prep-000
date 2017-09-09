9099c90a265393baeb4ad9fff9c78c72d16d1dec
Jennifer Stewart <jlgregg1@gmail.com>
2017-09-09 04:43:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function logWhisper(hello) {
   console.log('hello')
 }
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase) {
+  if (string === hello) {
     return "I can't hear you."
   }
-  else if (string === string.toUpperCase) {
+  else if (string === HELLO) {
     return "YES INDEED"
   }
   else if (string === "I love you, Grandma.") {
